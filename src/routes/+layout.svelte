0aa55b3adb31ec385fdf4d69cacc1508d4f9f438
Rick Chang <rickchang@Ricks-Mac-mini.local>
2024-04-02 23:03:34 +0800

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Header from './Header.svelte';
+	import './normalize.css';
+</script>
+
+<div class="app">
+	<!-- <Header /> -->
+
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+</style>
